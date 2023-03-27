c2111c8b3a0ccafef75bc45371496541e50546af
taweizhong <2748152219@qq.com>
2023-03-27 09:34:28 +0800

    v1

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -r ./organizations
+cp -r ../fabric-samples/test-network/organizations/ .
+
+
+export EXPLORER_CONFIG_FILE_PATH=./config.json
+export EXPLORER_PROFILE_DIR_PATH=./connection-profile
+export FABRIC_CRYPTO_PATH=./organizations
+
+docker-compose up -d
